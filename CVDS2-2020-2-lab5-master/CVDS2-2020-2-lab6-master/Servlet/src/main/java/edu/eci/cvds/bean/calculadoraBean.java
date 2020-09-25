1bb2239613fbc9261ba67091d8c142b0cb1652c4
Daniel1Varon <daniel.varon@mail.escuelaing.edu.co>
2020-09-24 23:50:03 -0500

    arreglo

--- CVDS2-2020-2-lab5-master/CVDS2-2020-2-lab6-master/Servlet/src/main/java/edu/eci/cvds/bean/calculadoraBean.java
+++ CVDS2-2020-2-lab5-master/CVDS2-2020-2-lab6-master/Servlet/src/main/java/edu/eci/cvds/bean/calculadoraBean.java
@@ -1,10 +1,7 @@
 package edu.eci.cvds.bean;
 
 import java.util.ArrayList;
-import java.util.Random;
 import javax.faces.bean.*;
-
-import java.util.Scanner;
 
 /**
 *
@@ -16,90 +13,102 @@ import java.util.Scanner;
 @ApplicationScoped
 //@SessionScoped
 public class calculadoraBean {
-	private ArrayList<Integer> datos=new ArrayList<Integer>();
-	private int prom=0;
-	private int numero=0;
+	private ArrayList<Double> datos=new ArrayList<Double>();
+	private double promedio;
 	private int indice;
-	private int valormoda;
-	Scanner reader = new Scanner(System.in);
+	private double valorModa;
+	
 	public calculadoraBean(){
-        restart();
-    }
-	public double calculateMean(ArrayList<Integer> promedio) {
-		//int[] valoresPromedio = int[] promedio;
-		for(int i=0; i<promedio.size();i++) {
-			prom+=promedio.get(i);
-		}
-		return prom=prom/promedio.size();
-	}
-	public void calculateStandardDeviation(ArrayList<Integer> dEstandar) {
-		double prome, sum = 0;
-		int n = dEstandar.size();
-		double desviacion;
-		prome = calculateMean(dEstandar);
-		for(int i=0; i<dEstandar.size();i++) {
-			sum += Math.pow (dEstandar.get(i)- prome, 2 );
-		}
-		desviacion = Math.sqrt (sum/( double)n);
+		valorModa=0;
 	}
 	
-	public void calculateVariance(ArrayList<Integer> varianza) {
+	public void addDato(String dato) {
+		String[] parts = dato.split(";");
+		double bucle;
+		for(int i=0;i<parts.length;i++) {
+			bucle=Double.parseDouble(parts[i]);
+			datos.add(bucle);
+		}
+		calculateMean();
+	}
+	
+	public double calculateMean() {
+		//int[] valoresPromedio = int[] promedio;
+		for(int i=0; i<datos.size();i++) {
+			promedio+=datos.get(i);
+		}
+		promedio=promedio/datos.size();
+		return promedio;
+	}
+	public double calculateStandardDeviation() {
+		double prome, sum = 0;
+		int n = datos.size();
+		double desviacion;
+		prome = calculateMean();
+		for(int i=0; i<datos.size();i++) {
+			sum += Math.pow (datos.get(i)- prome, 2 );
+		}
+		desviacion = Math.sqrt (sum/( double)n);
+		return desviacion;
+	}
+	
+	public double calculateVariance() {
 		double acMedia = 0, acMedia2 = 0;
-		for(int i=0; i<varianza.size();i++) {
-			acMedia = acMedia + varianza.get(i) ;
-			acMedia2 = acMedia2 + varianza.get(i) * varianza.get(i);
+		for(int i=0; i<datos.size();i++) {
+			acMedia = acMedia + datos.get(i) ;
+			acMedia2 = acMedia2 + datos.get(i) * datos.get(i);
 			
 		}
-		double valorVarianza = acMedia2 / (varianza.size()-1) - (acMedia * acMedia)/ (varianza.size()*(varianza.size()-1));
+		double valorVarianza = acMedia2 / (datos.size()-1) - (acMedia * acMedia)/ (datos.size()*(datos.size()-1));
+		return valorVarianza;
 	}
-	public void calculateMode(ArrayList<Integer> moda) {
+	public double calculateMode() {
 		int maximaVecesQueSeRepite = 0;
-		int valModa=0;
-		for(int i=0;i<moda.size();i++) {
+		double valModa=0;
+		for(int i=0;i<datos.size();i++) {
 			int vecesQueSeRepite = 0;
-			for(int j=0;j<moda.size();j++) {
-				if(moda.get(i)==moda.get(j)) {
+			for(int j=0;j<datos.size();j++) {
+				if(datos.get(i)==datos.get(j)) {
 					vecesQueSeRepite++;
 				}
 			}if(vecesQueSeRepite > maximaVecesQueSeRepite) {
-				valModa = moda.get(i);  //valor moda
+				valModa = datos.get(i);  //valor moda
 				maximaVecesQueSeRepite = vecesQueSeRepite;
 
 			}
-			valormoda=valModa;
-			
+			valorModa=valModa;
 		}
+		return valorModa;
 	}
-	public void restart(){
-        for(int i=0;i<10;i++) {
-        	numero = reader.nextInt();
-        	datos.add(numero);
-        }
-        
-    }
+	public void reiniciar() {
+		datos.clear();
+		promedio=0;
+		valorModa=0;
+	}
+	
 	public void menu() {
-		indice = reader.nextInt();
+		//indice = reader.nextInt();
 		if(indice==1) {
-			calculateMean(datos);
+			calculateMean();
 		}else if(indice == 2) {
-			calculateStandardDeviation(datos);
+			calculateStandardDeviation();
 		}else if(indice ==3) {
-			calculateVariance(datos);
+			calculateVariance();
 			
 		}else if(indice==4) {
-			calculateMode(datos);
+			calculateMode();
 		}else {
 			menu();
 		}
 		
 	}
-	public int getPromedio() {
-		return prom;
+	public double getPromedio() {
+		return promedio;
 	}
-	public ArrayList<Integer> getDatos(){
+	public ArrayList<Double> getDatos(){
 		return datos;
 	}
-	public int getmoda(){
-		return valormoda;
+	public double getmoda(){
+		return valorModa;
 	}
 }
